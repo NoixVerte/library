5e0de4963ee741bc51aad2cfbe09d1a66fb3c6bd
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-25 20:12:22 +0200

    Add ability to add books to library, input info

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const library = [];
-const body = document.querySelector("body");
+const libraryDiv = document.querySelector(".library");
+const addBookBtn = document.querySelector("#addBookBtn");
+const dialog = document.querySelector("dialog");
+const closeModalBtn = document.querySelector("#closeModalBtn");
+const submitBookBtn = document.querySelector("#submitBookBtn");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const readStatusInputs = document.getElementsByName("readStatus");
+let readStatusInput;
 
 function Book(name, author, pageAmount, hasBeenRead) {
     this.name = name;
@@ -14,30 +23,63 @@ function addBookToLibrary(name, author, pageAmount, hasBeenRead) {
 }
 
 function wipeShelves() {
-    while (body.hasChildNodes()) {
-        body.firstChild.remove();
+    while (libraryDiv.hasChildNodes()) {
+        libraryDiv.firstChild.remove();
     }
 }
 
 function updateShelves() {
     wipeShelves();
+    const fragment = document.createDocumentFragment();
     library.forEach(book => {
         const div = document.createElement("div");
         div.classList.add("book");
-        const name = document.createElement("p");
-        const author = document.createElement("p");
-        const pages = document.createElement("p");
-        const readStatus = document.createElement("p");
+        const name = document.createElement("h1");
+        const author = document.createElement("h2");
+        const pages = document.createElement("h3");
+        const readStatus = document.createElement("h4");
         name.innerText = book.name;
         author.innerText = book.author;
         pages.innerText = book.pageAmount + " pages";
         readStatus.innerText = book.hasBeenRead ? "Has been read" : "Has not been read";
         div.append(name, author, pages, readStatus);
-        body.append(div);
+        fragment.append(div);
     });
+    libraryDiv.append(fragment);
 }
 
-for (let i = 1; i <= 20; i++) {
+addBookBtn.addEventListener("click", ()  => {
+    titleInput.value  = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+    readStatusInput = null;
+    dialog.style.display = "flex";
+    dialog.showModal();
+});
+
+closeModalBtn.addEventListener("click", ()  => {
+    dialog.style.display = "none";
+    dialog.close();
+});
+
+submitBookBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    readStatusInputs.forEach(element => {
+        if (element.checked) {
+            if (element.getAttribute("value") == "true")  {
+                readStatusInput = true;
+            } else readStatusInput = false;
+        };
+    });
+    addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readStatusInput);
+    console.log(library);
+    dialog.style.display = "none";
+    dialog.close();
+    updateShelves();
+});
+
+
+for (let i = 1; i <= 5; i++) {
     addBookToLibrary(`Book ${i}`, `Author ${i}`, `${Math.floor(Math.random() * 250 + 101)}`, Math.random() > 0.5);
 }
 
